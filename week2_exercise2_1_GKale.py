19115a2b235136e7ca28c7e66d3d4ebbd1497ff7
Ganesh Kale <gkale@my365.bellevue.edu>
2021-03-27 10:14:41 -0700

    week2 assignment

---
+++ week2_exercise2_1_GKale.py
@@ -0,0 +1,47 @@
+# Class: DSC 520
+# Author: Ganesh Kale
+# Week - 2
+# Assignment - 2.1
+# Program to get python concepts refreshed
+
+# print hello world
+print('Hello World!!')
+
+# addition of two numbers
+a = 11
+b = 21
+print(f'Addition of two numbers {a} and {b} is: {a+b}')
+
+# subtraction of two numbers
+a = 105
+b = 51
+print(f'Subtraction of two numbers {a} and {b} is: {a-b}')
+
+# Multiplication of two numbers
+a = 25
+b = 26
+print(f'Multiplication of two numbers {a} and {b} is: {a*b}')
+
+# Division of two numbers
+a = 100
+b = 20
+print(f'Division of two numbers {a} and {b} is: {a/b}')
+
+# Concat two strings
+greet = 'Hello my name is '
+name = 'Ganesh'
+print(f'concatenated string is: {greet+name}')
+
+# List of 4 items
+my_list = [a, b, greet, name]
+print('List of 4 items: ', my_list)
+
+# append item to list - my_list
+last_name = 'Kale'
+my_list.append(last_name)
+print('Appended item to list: ', my_list)
+
+# Tuple with 4 items
+my_tup = (a, b, name, last_name)
+print('Tuple with 4 items: ', my_tup)
+
